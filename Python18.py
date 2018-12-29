0fa45d3178c27d54db8664cffbd25d7085ae31ab
Bristi97 <46235880+Bristi97@users.noreply.github.com>
2018-12-29 14:37:28 +0530

    Add files via upload

---
+++ Python18.py
@@ -0,0 +1,7 @@
+#Q18)program to find x to the power y without using standard functions
+num=int(input("enter a number"))
+num2=int(input("enter 2nd number"))
+a=1
+for num2 in range (1,num2+1):
+    a=(a*num)
+print(a)
