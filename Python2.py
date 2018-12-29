5ff021a7cf43b42c4a75054ca2b11ab24d52c095
Bristi97 <46235880+Bristi97@users.noreply.github.com>
2018-12-29 12:46:40 +0530

    Add files via upload

---
+++ Python2.py
@@ -0,0 +1,7 @@
+#Q2)program to accept n no.s and store them in array and find the max and min of the array?
+lst = []
+num = int(input('How many numbers: '))
+for n in range(num):
+    numbers = int(input('Enter number '))
+    lst.append(numbers)
+print("Maximum element in the list is :", max(lst), "\nMinimum element in the list is :", min(lst))
