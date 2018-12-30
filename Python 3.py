2012e22b411d5498b92d393cf7beba1f6321bd72
Bristi97 <46235880+Bristi97@users.noreply.github.com>
2018-12-30 21:39:52 +0530

    Add files via upload

---
+++ Python 3.py
@@ -0,0 +1,14 @@
+class Book:
+    def id1(self):
+        self.id_num=int(input("Please enter id\n"))
+    def cost(self):
+        self.book_cost=int(input("Please enter cost\n"))
+    def author(self):
+        self.auth_name=input("Please enter author\n")
+    def bookdb(self):
+        print("BOOK ID: {} BOOK COST:{} BOOK AUTHOR:{} ".format(self.id_num,self.book_cost,self.auth_name))
+b=Book()
+b.id1()
+b.cost()
+b.author()
+b.bookdb()
